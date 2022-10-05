7f120af9f06f10c3e3e63254e1f6c85580db5322
Aleksander Piasecki <aleksander.piasecki@vp.pl>
2022-10-05 21:37:38 +0200

    zmiany dodatkowe w '03' (cherry-pick)

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@ using std::cout;
 using std::endl;
 
 int policz(int lhs, int rhs) {
-    return lhs * rhs;
+    return (lhs + rhs) * (lhs - rhs);
 }
 
 int main() {
-    cout << "W drugim branchu; 2 * 3 = " << policz(2, 3) << endl;
+    cout << "W trzecim branchu; (2 + 3) * (2 - 3) = " << policz(2, 3) << endl;
     return 0;
 }
