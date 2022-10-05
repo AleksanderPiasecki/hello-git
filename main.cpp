ddf5c19d030cf105a87f0db3af11a424095d8f0b
Aleksander Piasecki <aleksander.piasecki@vp.pl>
2022-10-05 21:24:56 +0200

    zmiana na mnozenie w funkcji policz

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 using std::cout;
 using std::endl;
 
+int policz(int lhs, int rhs) {
+    return lhs * rhs;
+}
+
 int main() {
+    cout << "W drugim branchu; 2 * 3 = " << policz(2, 3) << endl;
     return 0;
 }
